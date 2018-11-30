ea931aec678ad4c4b41996829adf9481e2c64f1a
hschottm <hschottm@gmail.com>
2018-11-30 14:34:23 +0100

    Added requirements

--- src/Resources/contao/dca/tl_survey.php
+++ src/Resources/contao/dca/tl_survey.php
@@ -1,6 +1,6 @@
 <?php
 
-$found = (strlen(\Input::get('id'))) ? SurveyResultModel::findByPid(\Input::get('id')) : null;
+$found = (strlen(\Input::get('id'))) ? \Hschottm\SurveyBundle\SurveyResultModel::findByPid(\Input::get('id')) : null;
 $hasData = (null != $found && 0 < $found->numRows) ? true : false;
 
 /**
