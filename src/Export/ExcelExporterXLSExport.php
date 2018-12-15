a7168acc7b184b2ccaa2ec4c1dca89eb0d8d94fa
Helmut Schottmüller <helmut.schottmueller@me.com>
2018-12-15 15:41:30 +0100

    Fixed header

--- src/Export/ExcelExporterXLSExport.php
+++ src/Export/ExcelExporterXLSExport.php
@@ -1,87 +1,92 @@
 <?php
 
+/*
+ * @copyright  Helmut Schottmüller 2005-2018 <http://github.com/hschottm>
+ * @author     Helmut Schottmüller (hschottm)
+ * @package    contao-survey
+ * @license    LGPL-3.0+, CC-BY-NC-3.0
+ * @see	      https://github.com/hschottm/survey_ce
+ */
+
 namespace Hschottm\SurveyBundle\Export;
 
-use Hschottm\SurveyBundle\Export\Exporter;
 use Hschottm\ExcelXLSBundle\xlsexport;
 
 class ExcelExporterXLSExport extends Exporter
 {
-  public function __construct($type = self::EXPORT_TYPE_XLS)
-  {
-    parent::__construct($type);
-  }
-
-  public function createSpreadsheet()
-  {
-    $this->spreadsheet = new xlsexport();
-  }
-
-  public function setCellValue($sheet, $row, $col, $data)
-  {
-    if (array_key_exists($sheet, $this->sheets))
+    public function __construct($type = self::EXPORT_TYPE_XLS)
     {
-      $celldata = array(
+        parent::__construct($type);
+    }
+
+    public function createSpreadsheet()
+    {
+        $this->spreadsheet = new xlsexport();
+    }
+
+    public function setCellValue($sheet, $row, $col, $data)
+    {
+        if (array_key_exists($sheet, $this->sheets)) {
+            $celldata = [
         self::ROW => $row,
-        self::COL => $col
-      );
-      foreach ($data as $key => $value)
-      {
-        $celldata[$key] = $value;
-      }
-      if (!array_key_exists(self::CELLTYPE, $celldata)) $celldata[self::CELLTYPE] = self::CELLTYPE_STRING;
-      if ($celldata[self::CELLTYPE] === self::CELLTYPE_STRING)
-      {
-        $celldata[self::DATA] = utf8_decode($celldata[self::DATA]);
-      }
-      $this->sheets[$sheet][$this->getCell($row, $col)] = $celldata;
-      return true;
+        self::COL => $col,
+      ];
+            foreach ($data as $key => $value) {
+                $celldata[$key] = $value;
+            }
+            if (!array_key_exists(self::CELLTYPE, $celldata)) {
+                $celldata[self::CELLTYPE] = self::CELLTYPE_STRING;
+            }
+            if (self::CELLTYPE_STRING === $celldata[self::CELLTYPE]) {
+                $celldata[self::DATA] = utf8_decode($celldata[self::DATA]);
+            }
+            $this->sheets[$sheet][$this->getCell($row, $col)] = $celldata;
+
+            return true;
+        }
+
+        return false;
     }
-    else {
-      return false;
-    }
-  }
 
-  protected function setSpreadsheetProperties($title = "", $subject = "", $description = "", $creator = "", $modificator = "")
-  {
-    /*
-    $this->spreadsheet->getProperties()->setCreator($creator);
-    $this->spreadsheet->getProperties()->setLastModifiedBy($creator);
-    $this->spreadsheet->getProperties()->setTitle($title);
-    $this->spreadsheet->getProperties()->setSubject($subject);
-    $this->spreadsheet->getProperties()->setDescription($description);
-    */
-  }
-
-  protected function send()
-  {
-    $this->spreadsheet->sendFile(\StringUtil::sanitizeFileName(htmlspecialchars_decode($this->filename)).'.xls');
-    exit;
-  }
-
-  protected function setCellSpreadsheet($sheet, $cell)
-  {
-    $pos = $this->getCell($cell[self::ROW]+1, $cell[self::COL]);
-    $found = false;
-    foreach ($this->spreadsheet->worksheets as $sheetarray)
+    protected function setSpreadsheetProperties($title = '', $subject = '', $description = '', $creator = '', $modificator = '')
     {
-      if ($sheetarray['sheetname'] === utf8_decode($sheet)) $found = true;
+        /*
+        $this->spreadsheet->getProperties()->setCreator($creator);
+        $this->spreadsheet->getProperties()->setLastModifiedBy($creator);
+        $this->spreadsheet->getProperties()->setTitle($title);
+        $this->spreadsheet->getProperties()->setSubject($subject);
+        $this->spreadsheet->getProperties()->setDescription($description);
+        */
     }
 
-    if (!$found)
+    protected function send()
     {
-      $this->spreadsheet->addworksheet(utf8_decode($sheet));
+        $this->spreadsheet->sendFile(\StringUtil::sanitizeFileName(htmlspecialchars_decode($this->filename)).'.xls');
+        exit;
     }
-    $data = [
-      "sheetname" => utf8_decode($sheet),
-      "row" => $cell[self::ROW],
-      "col" => $cell[self::COL],
-      "data" => $cell[self::DATA]
+
+    protected function setCellSpreadsheet($sheet, $cell)
+    {
+        $pos = $this->getCell($cell[self::ROW] + 1, $cell[self::COL]);
+        $found = false;
+        foreach ($this->spreadsheet->worksheets as $sheetarray) {
+            if ($sheetarray['sheetname'] === utf8_decode($sheet)) {
+                $found = true;
+            }
+        }
+
+        if (!$found) {
+            $this->spreadsheet->addworksheet(utf8_decode($sheet));
+        }
+        $data = [
+      'sheetname' => utf8_decode($sheet),
+      'row' => $cell[self::ROW],
+      'col' => $cell[self::COL],
+      'data' => $cell[self::DATA],
     ];
-    $this->spreadsheet->setcell($data);
+        $this->spreadsheet->setcell($data);
 
-    switch ($cell[self::CELLTYPE])
-    {
+        switch ($cell[self::CELLTYPE]) {
       case CELLTYPE_STRING:
         $data['type'] = CELL_STRING;
         break;
@@ -96,19 +101,15 @@ class ExcelExporterXLSExport extends Exporter
         break;
     }
 
-    if (array_key_exists(self::BGCOLOR, $cell))
-    {
-      $data['bgcolor'] = $cell[self::BGCOLOR];
-    }
-    if (array_key_exists(self::COLOR, $cell))
-    {
-      $data['color'] = $cell[self::COLOR];
-    }
+        if (array_key_exists(self::BGCOLOR, $cell)) {
+            $data['bgcolor'] = $cell[self::BGCOLOR];
+        }
+        if (array_key_exists(self::COLOR, $cell)) {
+            $data['color'] = $cell[self::COLOR];
+        }
 
-    if (array_key_exists(self::ALIGNMENT, $cell))
-    {
-      switch ($cell[self::ALIGNMENT])
-      {
+        if (array_key_exists(self::ALIGNMENT, $cell)) {
+            switch ($cell[self::ALIGNMENT]) {
         case self::ALIGNMENT_H_GENERAL:
             $data['hallign'] = XLSXF_HALLIGN_GENERAL;
             break;
@@ -131,12 +132,10 @@ class ExcelExporterXLSExport extends Exporter
             $data['hallign'] = XLSXF_HALLIGN_CACROSS;
             break;
       }
-    }
+        }
 
-    if (array_key_exists(self::TEXTROTATE, $cell))
-    {
-      switch ($cell[self::TEXTROTATE])
-      {
+        if (array_key_exists(self::TEXTROTATE, $cell)) {
+            switch ($cell[self::TEXTROTATE]) {
         case self::TEXTROTATE_CLOCKWISE:
           $data['textrotate'] = XLSXF_TEXTROTATION_CLOCKWISE;
           break;
@@ -146,32 +145,28 @@ class ExcelExporterXLSExport extends Exporter
         case self::TEXTROTATE_NONE:
           break;
       }
-    }
+        }
 
-    if (array_key_exists(self::TEXTWRAP, $cell))
-    {
-      if ($cell[self::TEXTWRAP]) $data['textwrap'] = '1';
-    }
+        if (array_key_exists(self::TEXTWRAP, $cell)) {
+            if ($cell[self::TEXTWRAP]) {
+                $data['textwrap'] = '1';
+            }
+        }
 
-    if (array_key_exists(self::COLWIDTH, $cell))
-    {
-      $data['colwidth'] = $cell[self::COLWIDTH];
-    }
+        if (array_key_exists(self::COLWIDTH, $cell)) {
+            $data['colwidth'] = $cell[self::COLWIDTH];
+        }
 
-    if (array_key_exists(self::FONTWEIGHT, $cell))
-    {
-      switch ($cell[self::FONTWEIGHT])
-      {
+        if (array_key_exists(self::FONTWEIGHT, $cell)) {
+            switch ($cell[self::FONTWEIGHT]) {
         case self::FONTWEIGHT_BOLD:
           $data['fontweight'] = XLSFONT_BOLD;
           break;
       }
-    }
+        }
 
-    if (array_key_exists(self::BORDERBOTTOM, $cell))
-    {
-      switch ($cell[self::BORDERBOTTOM])
-      {
+        if (array_key_exists(self::BORDERBOTTOM, $cell)) {
+            switch ($cell[self::BORDERBOTTOM]) {
         case self::BORDER_THIN:
           $data['borderbottom'] = XLSXF_BORDER_THIN;
           break;
@@ -215,17 +210,14 @@ class ExcelExporterXLSExport extends Exporter
           $data['borderbottom'] = XLSXF_BORDER_NOBORDER;
           break;
       }
-    }
+        }
 
-    if (array_key_exists(self::BORDERBOTTOMCOLOR, $cell))
-    {
-      $data['borderbottomcolor'] = $cell[self::BORDERBOTTOMCOLOR];
-    }
+        if (array_key_exists(self::BORDERBOTTOMCOLOR, $cell)) {
+            $data['borderbottomcolor'] = $cell[self::BORDERBOTTOMCOLOR];
+        }
 
-    if (array_key_exists(self::BORDERTOP, $cell))
-    {
-      switch ($cell[self::BORDERTOP])
-      {
+        if (array_key_exists(self::BORDERTOP, $cell)) {
+            switch ($cell[self::BORDERTOP]) {
         case self::BORDER_THIN:
           $data['bordertop'] = XLSXF_BORDER_THIN;
           break;
@@ -269,17 +261,14 @@ class ExcelExporterXLSExport extends Exporter
           $data['bordertop'] = XLSXF_BORDER_NOBORDER;
           break;
       }
-    }
+        }
 
-    if (array_key_exists(self::BORDERTOPCOLOR, $cell))
-    {
-      $data['bordertopcolor'] = $cell[self::BORDERTOPCOLOR];
-    }
+        if (array_key_exists(self::BORDERTOPCOLOR, $cell)) {
+            $data['bordertopcolor'] = $cell[self::BORDERTOPCOLOR];
+        }
 
-    if (array_key_exists(self::BORDERLEFT, $cell))
-    {
-      switch ($cell[self::BORDERLEFT])
-      {
+        if (array_key_exists(self::BORDERLEFT, $cell)) {
+            switch ($cell[self::BORDERLEFT]) {
         case self::BORDER_THIN:
           $data['borderleft'] = XLSXF_BORDER_THIN;
           break;
@@ -323,17 +312,14 @@ class ExcelExporterXLSExport extends Exporter
           $data['borderleft'] = XLSXF_BORDER_NOBORDER;
           break;
       }
-    }
+        }
 
-    if (array_key_exists(self::BORDERLEFTCOLOR, $cell))
-    {
-      $data['borderleftcolor'] = $cell[self::BORDERLEFTCOLOR];
-    }
+        if (array_key_exists(self::BORDERLEFTCOLOR, $cell)) {
+            $data['borderleftcolor'] = $cell[self::BORDERLEFTCOLOR];
+        }
 
-    if (array_key_exists(self::BORDERRIGHT, $cell))
-    {
-      switch ($cell[self::BORDERRIGHT])
-      {
+        if (array_key_exists(self::BORDERRIGHT, $cell)) {
+            switch ($cell[self::BORDERRIGHT]) {
         case self::BORDER_THIN:
           $data['borderright'] = XLSXF_BORDER_THIN;
           break;
@@ -377,30 +363,25 @@ class ExcelExporterXLSExport extends Exporter
           $data['borderright'] = XLSXF_BORDER_NOBORDER;
           break;
       }
-    }
+        }
 
-    if (array_key_exists(self::BORDERRIGHTCOLOR, $cell))
-    {
-      $data['borderrightcolor'] = $cell[self::BORDERRIGHTCOLOR];
-    }
+        if (array_key_exists(self::BORDERRIGHTCOLOR, $cell)) {
+            $data['borderrightcolor'] = $cell[self::BORDERRIGHTCOLOR];
+        }
 
-    if (array_key_exists(self::MERGE, $cell))
-    {
-      $range = $cell[self::MERGE];
-      $rangeArray = $this->getArrayFromRange($range);
-      $this->spreadsheet->merge_cells($sheet, $rangeArray[0], $rangeArray[2], $rangeArray[1], $rangeArray[3]);
-    }
+        if (array_key_exists(self::MERGE, $cell)) {
+            $range = $cell[self::MERGE];
+            $rangeArray = $this->getArrayFromRange($range);
+            $this->spreadsheet->merge_cells($sheet, $rangeArray[0], $rangeArray[2], $rangeArray[1], $rangeArray[3]);
+        }
 
-    if (array_key_exists(self::FONTSTYLE, $cell))
-    {
-      switch ($cell[self::FONTSTYLE])
-      {
+        if (array_key_exists(self::FONTSTYLE, $cell)) {
+            switch ($cell[self::FONTSTYLE]) {
         case self::FONTSTYLE_ITALIC:
           $data['fontstyle'] = XLSFONT_STYLE_ITALIC;
           break;
       }
+        }
+        $this->spreadsheet->setcell($data);
     }
-    $this->spreadsheet->setcell($data);
-  }
-
 }
