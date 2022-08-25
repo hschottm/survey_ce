bc50c9b1b068c82c5d79a38a7468930fa7845b6a
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2022-08-25 10:10:12 +0000

    Update tl_survey_page.php

--- src/Resources/contao/dca/tl_survey_page.php
+++ src/Resources/contao/dca/tl_survey_page.php
@@ -188,11 +188,11 @@ class tl_survey_page extends Backend
      */
     public function getSurveyTemplates(DataContainer $dc)
     {
-        if (version_compare(VERSION.BUILD, '2.9.0', '>=')) {
+        /**if (version_compare(VERSION.BUILD, '2.9.0', '>=')) { */
             return $this->getTemplateGroup('survey_', $dc->activeRecord->pid);
-        }
+    /**    }
 
-        return $this->getTemplateGroup('survey_');
+        return $this->getTemplateGroup('survey_'); *//
     }
 
     /**
